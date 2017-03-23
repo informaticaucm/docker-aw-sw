16313c68a8798d293a99f4604b9fdfeeef55a2bf
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2017-03-23 17:47:15 +0100

    Fixing typo

--- apache/fix-acl.sh
+++ apache/fix-acl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find /var/www/html -type d -exec chmod 770 {} \;
-find /var/www/html -type f -exec chdmo 660 {} \;
+find /var/www/html -type f -exec chmod 660 {} \;
 chown -R www-data: /var/www/html
