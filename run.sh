0dad7bd96aed9c52c126407249b715cc0a829dad
e-ucm <e-ucm@xenial-test.internal.e-ucm.es>
2018-02-03 14:24:54 +0100

    Actualizando PHP a la version 7.2. Reorganización del Dockerfile.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #PHP
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.0/apache2/php.ini
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.2/apache2/php.ini
 
 #SSH server
 echo "root:${SSH_PASS}" | chpasswd
@@ -19,10 +19,11 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo 'Database initialized'
 
     echo "=> Done!"  
-    configure_mysql.sh
+    /usr/local/bin/configure_mysql.sh
 else
     echo "=> Using an existing volume of MySQL"
 fi
 
+/usr/local/bin/configure_phpmyadmin.sh
 
-exec supervisord -n
+exec supervisord -n -c /etc/supervisor/supervisord.conf
